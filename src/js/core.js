8a5a55a4dfcde7e85dd95941c465355baa02dc5b
regdoug <pierce.reginald@gmail.com>
2014-12-23 14:53:25 -0500

    Reorganized styles
    The SASS files are now organized better and superfluous files were
    deleted. The base configuration and Foundation components are
    included in `_base.scss` and the additions specific to this theme
    are in `_players.scss`.  `style.scss` has the Wordpress theme header
    and includes `_base.scss` and `_players.scss`

--- src/js/core.js
+++ src/js/core.js
@@ -1,11 +1,29 @@
 /* Custom functions */
+
+/* Set up foundation off canvas menu */
+jQuery(document).foundation({
+    offcanvas: {
+        open_method: 'overlap',
+        close_on_click: false
+    }
+});
+
+/* When the user scrolls down, #logo needs to be hidden so it doesn't block content */
 jQuery(function(){
-    jQuery(document).on('scroll',function(){
+    jQuery(document).on('scroll', function() {
         var scrollTop = jQuery(document).scrollTop();
-        if(scrollTop == 0){
-            jQuery('#logo').removeClass('rotate-away');
+        if(scrollTop === 0){
+            if( Modernizr.csstransforms3d ) {
+                jQuery('#logo').removeClass('rotate-away');
+            } else {
+                jQuery('#logo').show(600);
+            }
         } else {
-            jQuery('#logo').addClass('rotate-away');
+            if( Modernizr.csstransforms3d ) {
+                jQuery('#logo').addClass('rotate-away');
+            } else {
+                jQuery('#logo').hide(600);
+            }
         }
     });
 });
